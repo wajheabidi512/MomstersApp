b125e5c0b05ad95f8c972592120efc35efbd6711
wajheabidi <wajheabidi@yahoo.com>
2020-08-06 16:55:52 +0500

    Updates

---
+++ precache-manifest.ecbf8b723957114c1c4f02095ead20af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7568e7805a782d80f5383a8ffba19759",
+    "url": "/MomstersApp/index.html"
+  },
+  {
+    "revision": "13c9d1f764d177f2b9cc",
+    "url": "/MomstersApp/static/css/main.83afed7c.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/MomstersApp/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MomstersApp/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c9d1f764d177f2b9cc",
+    "url": "/MomstersApp/static/js/main.3c1c591d.chunk.js"
+  },
+  {
+    "revision": "17dceecf0a22edb5d272",
+    "url": "/MomstersApp/static/js/runtime-main.bfe4d1e9.js"
+  }
+]);
